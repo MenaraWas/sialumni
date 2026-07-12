a665e243f5f90e2c4f0674bb37f66a0ae7db9f51
Menarawas <menaralintang.2019@student.uny.ac.id>
2026-07-12 19:01:23 +0700

    add tracerpubliccontroller and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TracerPublicController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tracer', function () {
+    return view('tracer.landing');
+})->name('tracer.landing');
+
+Route::prefix('tracer')->name('tracer.')->group(function () {
+    
+    Route::get('/', [TracerPublicController::class, 'landing'])->name('landing');
+    Route::get('/isi', [TracerPublicController::class, 'form'])->name('form');
+    Route::post('/isi', [TracerPublicController::class, 'submit'])->name('submit');
+    Route::get('/terima-kasih', [TracerPublicController::class, 'thankyou'])->name('thankyou');
+
+});
+
+
