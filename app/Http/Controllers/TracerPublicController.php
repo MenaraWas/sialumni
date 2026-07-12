ba1a8c1b8b21f886e2800b7e3cc9f90c289359c3
Menarawas <menaralintang.2019@student.uny.ac.id>
2026-07-12 19:08:56 +0700

    first init controller tracer public

--- app/Http/Controllers/TracerPublicController.php
+++ app/Http/Controllers/TracerPublicController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class TracerPublicController extends Controller
 {
-    //
+    public function landing(){
+        return view('tracer.landing');
+    }
+
+    public function form(){
+        return view('tracer.form');
+    }
+
+    public function submit(Request $request){
+
+    }
+
+    public function thankyou(){
+        return view('tracer.thankyou');
+    }
 }
